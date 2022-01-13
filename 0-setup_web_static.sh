e2f7bb7810079795d7549e921763cd5e34bdb343
Sergio Vera <sergio.vera@icloud.com>
2022-01-13 16:05:14 +0000

    twaeking code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,14 +5,8 @@ apt-get -y update
 apt-get install nginx -y
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
-echo "<html>
-        <head>
-        </head>
-        <body>
-          Holberton School
-        </body>
-</html>" >> /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
+echo "This better work" | sudo tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
 sed -i '49i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 service nginx restart

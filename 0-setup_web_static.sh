9033e67f3475884712c3e32af16b5ba45267c6a8
Sergio Vera <sergio.vera@icloud.com>
2022-01-13 20:40:00 +0000

    setting load balancer

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 # Setup web servers for the deployment of web_static
-
 sudo apt-get -y update
 sudo apt-get install nginx -y
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 sudo echo "This better work" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '49i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-service nginx restart
+sudo service nginx restart

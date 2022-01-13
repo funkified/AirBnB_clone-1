ca8b12f72ddaa9ed8fd7dd5038617ff44ccd8f2f
Sergio Vera <sergio.vera@icloud.com>
2022-01-13 17:52:13 +0000

    twaeking code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Setup web servers for the deployment of web_static
 
-apt-get -y update
-apt-get install nginx -y
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
-echo "This better work" | sudo tee /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -hR ubuntu:ubuntu /data/
-sed -i '49i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo apt-get -y update
+sudo apt-get install nginx -y
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+sudo echo "This better work" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '49i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 service nginx restart

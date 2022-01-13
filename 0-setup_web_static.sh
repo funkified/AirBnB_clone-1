6460a9cf124e5dedfaf9a5c17b483914457ad042
Sergio Vera <sergio.vera@icloud.com>
2022-01-13 04:42:16 +0000

    shellcheck

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ echo "<html>
         <head>
         </head>
         <body>
-                Holberton School
+          Holberton School
         </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current

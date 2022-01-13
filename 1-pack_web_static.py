eeba7e3fa7e3e3cacbbe2425990653619e67a31e
Sergio Vera <sergio.vera@icloud.com>
2022-01-13 15:52:43 +0000

    twaeking code

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,19 +5,17 @@ the web_static folder of your AirBnB Clone repo, using the function do_pack
 from datetime import datetime
 from fabric.api import local
 from fabric import *
-from os.path import isdir
 
 
 def do_pack():
     """
     return the archive has been correctly generated
     """
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_path = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_path))
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    local("mkdir versions")
+    file_path = "versions/web_static_{}.tgz".format(date)
+    exec_ = local("sudo tar -cvzf {} web_static".format(file_path))
+    if exec_.succeeded:
         return file_path
-    except Exception:
+    else:
         return None

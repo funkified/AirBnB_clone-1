fd7d66f53f19cbe3b15db209009ef053fc586196
Sergio Vera <sergio.vera@icloud.com>
2022-01-14 02:33:18 +0000

    adding upgrade to script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 # Setup web servers for the deployment of web_static
-
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+echo "Thie better works" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '45i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
